2817619e06f14c31060d6f8a25b3e6b7013c3e68
pci <aswq233333@naver.com>
2023-04-25 14:09:33 +0900

    map 사용 및 props 사용법

--- src/App.js
+++ src/App.js
@@ -12,8 +12,9 @@ function App() {
   // useState 는 중간에 값이 변경되거나 바뀌면 자동으로 렌더링이 돼서 html 에 반영됨 (재 렌더링)
   // 따라서 useState 는 데이터 변동이 있을 시 자동으로 html 에 반영되게 하고 싶을 때 사용 (자주 변경되는)
   let [title, setTitle] = useState(['남자코트추천', '강남맛집', '파이썬독학']);
-  let [like, setLike] = useState(0);
+  let [like, setLike] = useState([0,0,0]);
   let [modal, setModal] = useState(false); // 모달창 디폴트 false
+  let [modalTitle, setModalTitle] = useState(0); // state 위치를 굳이 여기에 만들어야 하는 이유는 98 라인 확인
 
   return (
     <div className="App">
@@ -35,21 +36,28 @@ function App() {
         setTitle(copy);
         }}>글수정</button>
 
-      <div className='list'>
-        <h4 className='hover'> { title[0] } <span className='hover' onClick={ ()=>{ setLike(like + 1) }}>❤️</span> { like } </h4>
-        <p>2023년 발행</p>
-      </div>
+      {
+        // map() 반복문 함수 => 글제목 title array 갯수만큼 반복문
+        // return 안에 있는 것들을 array 로 담아줌
+        // 유용한 파라미터 2개 사용 가능
+        // a 파라미터 => array 안에 있던 데이터
+        // i 파라미터 => 반복문이 돌 때 마다 1씩 증가하는 정수
+        title.map(function(a, i) {
+          return (
+            <div className='list' key={ i }>
+              <h4 onClick={ () => { setModal(!modal); setModalTitle(i) }}> { title[i] }
+                <span onClick={ ()=> {
+                  let copy = [...like];
+                  copy[i] = copy[i] + 1;
+                  setLike(copy)}}>❤️
+                </span> { like[i] }
+              </h4>
+              <p>2023년 발행</p>
+            </div>
+          )
+        })
+      }
 
-      <div className='list'>
-        <h4 className='hover'>{ title[1] }</h4>
-        <p>2023년 발행</p>
-      </div>
-
-      <div className='list'>
-        <h4 className='hover' onClick={ () => { setModal(!modal) }}>{ title[2] }</h4>
-        <p>2023년 발행</p>
-      </div>
-       
       {/* <Component>/</Component> 아래와 같은 방법으로도 사용 가능함 */}
       {/* 동적인 UI 만드는 방법 
         1. html css 미리 디자인 완성
@@ -57,7 +65,12 @@ function App() {
         3. state 에 따라 UI 가 어떻게 보일지 작성 */}
         {
           // 삼항연산자
-          modal === true ? <Modal/> : null
+          // 부모 state 를 자식에게도 사용하게 해주고 싶다면(props) 2번의 step 이 있음
+          // 컴포넌트 사용하는 곳에서 자유롭게 작명 해주고 사용하고싶은 state 이름을 적으면 됨
+          // 보통 작명은 사용하고자 하는 state 와 동일하게 작명 하는 경우가 있음
+          // 자식 컴포넌트 Modal() 함수에서 파라미터를 추가해줌 보통 props 라고 작명을 많이 함
+          // props 전송 사용 방법은 부모 -> 자식만 가능하고 자식 -> 부모는 안됨
+          modal === true ? <Modal modalTitle={ modalTitle } title={ title } /> : null
         }
 
     </div>
@@ -74,17 +87,21 @@ function App() {
 // 1. 반복적인 UI 를 사용하여야 할 때 사용
 // 2. 자주 변경되는 UI 
 // 3. 큰 페이지들 또한 컴포넌트 사용하는게 좋음
-
 // 단점
 // 1. state 를 가져다가 사용할 때 문제가 생김
+// state 를 가져다 쓰려면 props 방식을 사용하면 됨
 // 변수 선언을 const 로 하게되면 console 에 에러 메세지가 찍혀서 좋음
-const Modal = () => {
+function Modal(props) {
+  // let [modalTitle, setModalTitle] = useState(0); 이런식으로 여기에 선언해도 되지만
   return (
     <>
-      <div className='modal'>
-        <h4>제목</h4>
+      <div className='modal' style={{ background : 'orange' }}>
+        <h4>{ props.title[props.modalTitle] }</h4>
+        {/** <h4>{ props.title[modalTitle] }</h4> 95번 라인에 state 선언 후 사용해도 되지만,
+          선언한 state가 Modal, App 등등 여러 함수 컴포넌트들에 필요하다면 가장 상위 컴포넌트에 보관하는게 좋음 패륜전송 불륜전송 X */} 
         <p>날짜</p>
         <p>상세내용</p>
+        <button>글수정</button>
       </div>
     </>
   )
